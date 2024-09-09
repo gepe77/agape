bbca853186f35b7bb8e10315088d37329e9cf30c
gepe77 <villajixd@gmail.com>
2024-09-09 19:00:39 +0700

    commit

---
+++ phyton/quiz game/quiz.py
@@ -0,0 +1,10 @@
+point = 0
+print("welcome to quiz that maked by agape tampan 77")
+print("\nnote:if you answer with wrong spelling you will get no point")
+
+print("lets start with first question")
+pertanyaan1 = input("who is the founder of america continent? ")
+if pertanyaan1.lower() == "christoper colombus":
+  print(point + 1 ,"+1 point your great at this")
+else:
+  print("get lucky next time! +0")
