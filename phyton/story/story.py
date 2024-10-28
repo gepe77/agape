42744c7d70928445228fb49eeace21657539cfb5
gepe77 <villajixd@gmail.com>
2024-10-28 18:53:05 +0700

    commit

---
+++ phyton/story/story.py
@@ -0,0 +1,9 @@
+import random
+
+when = ["last night","yesterday","last year","last month"] #square brackets adalah list fungsinya sama seperti brackets bedanya square brackets digunakan untuk melakukan list variable
+who = ["my uncle","my sister","anton", "my mom", "kiel"]
+doing = ["ate a", "drove a", "threw a", "smacked a","saw a", "played a", "fought a"]
+object = ["car","book","toilet paper","guitar","crown","somebody","toy"] 
+until = ["until"]
+happened = ["it explode","nothing happened","my mom sell the car","MCR reunited","my phone rang"]
+print(random.choice(when),random.choice(who),random.choice(doing),random.choice(object),random.choice(until),random.choice(happened))
