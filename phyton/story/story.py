4cf09f75622b26f73c6a33ffcd91ca23105368de
gepe77 <villajixd@gmail.com>
2024-11-08 17:56:27 +0700

    commit

--- phyton/story/story.py
+++ phyton/story/story.py
@@ -1,6 +1,6 @@
 import random
 
-when = ["last night","yesterday","last year","last month"] #square brackets adalah list fungsinya sama seperti brackets bedanya square brackets digunakan untuk melakukan list variable
+when = ["last night","yesterday","last year","last month"] #list menggunakan square
 who = ["my uncle","my sister","anton", "my mom", "kiel"]
 doing = ["ate a", "drove a", "threw a", "smacked a","saw a", "played a", "fought a"]
 object = ["car","book","toilet paper","guitar","crown","somebody","toy"] 
---
+++ phyton/temperature.py
@@ -0,0 +1,12 @@
+#sigma = int(input("masukan suhu celcius")) 
+#function adalah cara yang digunakan untuk mempermudah proses koding dimana coder menggunakan function untuk mempercepat membuat script
+#def convert(): #nama dari function langkah pertama membuat function
+ #   print((sigma * 9/5) + 32)#definisi langkah ke 2
+#convert()#pemanggil function langkah ke 3
+#sekian cara cara menggunakan function
+
+
+
+def convert(sigma): #sigma adalah parameter
+    print((sigma * 9/5) + 32)
+convert(2)#angka 2 adalah argumen
