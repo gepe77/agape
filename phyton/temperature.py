4ce267a023b4f89c4d03e4abd740bd9fe6a40892
gepe77 <villajixd@gmail.com>
2024-11-22 17:43:30 +0700

    commit

--- phyton/temperature.py
+++ phyton/temperature.py
@@ -1,12 +1,36 @@
-#sigma = int(input("masukan suhu celcius")) 
-#function adalah cara yang digunakan untuk mempermudah proses koding dimana coder menggunakan function untuk mempercepat membuat script
+#sigma = int(input("masukan suhu celcius = ")) 
+#function #adalah cara yang digunakan untuk mempermudah proses koding dimana coder menggunakan function untuk mempercepat membuat script
 #def convert(): #nama dari function langkah pertama membuat function
- #   print((sigma * 9/5) + 32)#definisi langkah ke 2
+#    print((sigma * 9/5) + 32)#definisi langkah ke 2
 #convert()#pemanggil function langkah ke 3
 #sekian cara cara menggunakan function
 
 
 
-def convert(sigma): #sigma adalah parameter
-    print((sigma * 9/5) + 32)
-convert(2)#angka 2 adalah argumen
+def convert(sigma): #sigma adalah parameter parameter adalah variabel didalam tanda kurung
+       print((sigma * 9/5) + 32)
+
+
+convert(2)#angka 2 adalah argumen arguman adalah nilai yang dikirim ke fungsi saat di panggil
+convert(3)#angka 2 adalah argumen
+convert(4)#angka 2 adalah argumen
+convert(5)#angka 2 adalah argumen
+
+
+def jeff(hellnah):
+    print((hellnah * 9/5) + 491.67)
+jeff(0)
+
+
+def hehe(hehey):
+    print((hehey) + 273,15)
+
+
+hehe(7)
+hehe(8)
+hehe(9)
+hehe(10)
+hehe(11)
+
+convert(783)
+hehe(19)
